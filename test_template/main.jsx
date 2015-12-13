a12f0a8c7fefd458d4934a41cac57dc44443f7c2
miziomon <maurizio@mavida.com>
2015-12-13 16:13:15 +0100

    add test template

---
+++ test_template/main.jsx
@@ -0,0 +1,22 @@
+
+
+var Component = React.createClass({
+
+
+
+  render: function() {
+    return (
+      <div >
+           
+        <h1>Component</h1>
+       
+        
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Component  />,
+  document.getElementById('content')
+);
