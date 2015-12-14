ffa5ffb3dc764381c1cedce079181f98b35fae2c
miziomon <maurizio@mavida.com>
2015-12-14 14:52:46 +0100

    test02 - component isolation, central component to delivery event, component reference callback

--- test02/build/main.js
+++ test02/build/main.js
@@ -1,38 +1,121 @@
+// ------------ box ------------- //
+var ComponentBox = React.createClass({
+    displayName: "ComponentBox",
 
+    handleComponentClick: function (component) {
+        console.log("componet click ");
+        console.log(component);
 
-var Component = React.createClass({
-  displayName: "Component",
+        this.refs['overlay'].active();
+    },
 
-  getInitialState: function () {
-    return { class: this.props.class };
-  },
-
-  handleClick: function (e) {
-    e.preventDefault();
-
-    console.log("click " + this.state.class);
-
-    this.setState({ class: "active" });
-  },
-
-  render: function () {
-    return React.createElement(
-      "div",
-      { className: this.state.class },
-      React.createElement(
-        "h1",
-        null,
-        "Component"
-      ),
-      React.createElement(
-        "a",
-        { onClick: this.handleClick },
-        "active"
-      )
-    );
-  }
+    render: function () {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(Component, { "class": "red", onComponentClick: this.handleComponentClick }),
+            React.createElement(Component, { "class": "green", onComponentClick: this.handleComponentClick }),
+            React.createElement(Overlay, { ref: "overlay", "class": "overlay" })
+        );
+    }
 });
 
-ReactDOM.render(React.createElement(Component, { "class": "red" }), document.getElementById('content_a'));
+// ------------ component  ------------- //
 
-ReactDOM.render(React.createElement(Component, { "class": "green" }), document.getElementById('content_b'));
+var Component = React.createClass({
+    displayName: "Component",
+
+    getInitialState: function () {
+        return {
+            class: this.props.class,
+            text: ""
+        };
+    },
+
+    handleClick: function (e) {
+        e.preventDefault();
+
+        //this.setState({class: "active"});
+
+        this.props.onComponentClick(this.state.class);
+
+        console.log("click " + this.state.class);
+    },
+
+    handleChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { className: this.state.class },
+            React.createElement(
+                "h1",
+                null,
+                "Component"
+            ),
+            React.createElement("input", { type: "text", onChange: this.handleChange }),
+            React.createElement(
+                "p",
+                null,
+                this.state.text
+            ),
+            React.createElement(
+                "a",
+                { onClick: this.handleClick },
+                "active"
+            )
+        );
+    }
+});
+
+// ------------ overlay  ------------- //
+
+var Overlay = React.createClass({
+    displayName: "Overlay",
+
+    getInitialState: function () {
+        return {
+            class: this.props.class,
+            text: "",
+            content: ""
+        };
+    },
+
+    active: function () {
+        console.log("partito");
+        this.setState({ class: "overlay active" });
+        this.setState({ content: "Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, " });
+    },
+
+    handleClose: function (e) {
+
+        e.preventDefault();
+
+        if (e.target.className == "overlay active" || e.target.parentNode.className == "overlay-close") {
+            this.setState({ class: "overlay" });
+            this.setState({ content: "" });
+        }
+    },
+
+    render: function () {
+        return React.createElement(
+            "div",
+            { className: this.state.class, onClick: this.handleClose },
+            React.createElement(
+                "div",
+                { className: "overlay-content" },
+                this.state.content
+            ),
+            React.createElement(
+                "a",
+                { className: "overlay-close", onClick: this.handleClose },
+                React.createElement("i", { className: "fa fa-times" }),
+                " Close"
+            )
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(ComponentBox, null), document.getElementById('content_a'));
--- test02/main.jsx
+++ test02/main.jsx
@@ -1,29 +1,74 @@
+// ------------ box ------------- //
+var ComponentBox = React.createClass({
 
+
+    
+
+    handleComponentClick: function( component ) {
+        console.log("componet click ");
+        console.log( component );
+
+        this.refs['overlay'].active();
+
+    },
+
+    render: function() {
+        return (
+
+        <div>
+        <Component class="red" onComponentClick={this.handleComponentClick}/>
+        <Component class="green" onComponentClick={this.handleComponentClick} />
+        <Overlay ref="overlay" class="overlay" />
+
+        </div>
+        );
+      }
+});
+
+
+
+
+
+// ------------ component  ------------- //
 
 var Component = React.createClass({
 
 
 getInitialState: function() {
-    return {class: this.props.class};
+    return {
+        class: this.props.class,
+        text: ""
+        };
   },
 
 handleClick: function(e) {
     e.preventDefault();
     
+    //this.setState({class: "active"});
     
     
     
-    this.setState({class: "active"});
+    this.props.onComponentClick( this.state.class );
+
+    
+
     console.log( "click " + this.state.class );
   },
 
+
+handleChange: function(e) {
+    this.setState({text: e.target.value });
+
+
+},
 
   render: function() {
     return (
       <div className ={ this.state.class }>
            
         <h1 >Component</h1>
-       
+        <input type="text" onChange={this.handleChange} />
+        <p>{this.state.text}</p>
         <a onClick={this.handleClick}>active</a>
         
       </div>
@@ -31,13 +76,49 @@ handleClick: function(e) {
   }
 });
 
+
+// ------------ overlay  ------------- //
+
+var Overlay = React.createClass({
+
+    getInitialState: function() {
+        return {
+            class: this.props.class,
+            text: "",
+            content: ""
+            };
+      },
+
+    active: function(){
+        console.log("partito");
+        this.setState({class: "overlay active"});
+        this.setState({content: "Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, "});
+    },
+
+    handleClose: function( e ){
+        e.preventDefault();
+        if ( e.target.className == "overlay active" || e.target.parentNode.className  == "overlay-close" ) {
+            this.setState({class: "overlay"});
+            this.setState({content: ""});
+        }
+    },
+
+    render: function() {
+        return (
+        <div className={ this.state.class } onClick={this.handleClose}>
+            <div className="overlay-content" >{this.state.content}</div>
+            <a  className="overlay-close" onClick={this.handleClose}>
+                <i className="fa fa-times"></i> Close</a>        
+
+        </div>
+        );
+      }
+});
+
 ReactDOM.render(
-  <Component class="red" />,
+  <ComponentBox />,
   document.getElementById('content_a')
 );
 
 
-ReactDOM.render(
-  <Component class="green" />,
-  document.getElementById('content_b')
-);
+
